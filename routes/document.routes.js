d0710518a6cd46d5c5b1644cc383de6e1bc8d5a8
primeOmar <omarbusolo@gmail.com>
2026-05-03 20:52:17 +0300

    update documents upload

--- routes/document.routes.js
+++ routes/document.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { requireAuth } from '../middleware/auth.middleware.js';
-import { parseDocumentData, uploadDocumentsToR2 } from '../middleware/Uploadtocloudflare.js';
+import { parseDocumentData, uploadDocumentsToR2 } from '../middleware/uploads/Uploadtocloudflare.js';
 
 const router = express.Router();
 
